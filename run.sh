7856d5a41bd300f93bc1a8d14ce6726e3927516a
Jason Vasquez <jason.vasquez@developertown.com>
2015-11-18 11:44:31 -0500

    Enhancement to launch process

--- run.sh
+++ run.sh
@@ -20,4 +20,6 @@ output=$(go install $PACKAGES 2>&1) && echo "done." || {
 }
 
 echo "Running app..."
-exec $GOPATH/bin/meetgrinder-api
+echo "Launching app at http://$(docker-machine ip default):8081"
+docker build -t meetgrinder-api .
+exec docker run -p 8081:8081 --link=meetgrinder-db:db --name meetgrinder-api --rm meetgrinder-api
