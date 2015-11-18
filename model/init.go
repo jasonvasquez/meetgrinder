8dae6d7438755fd57be97891fa431bb7427d58fe
Jason Vasquez <jason.vasquez@developertown.com>
2015-11-18 07:21:51 -0500

    Connect to mongo inside and outside of docker environments

--- model/init.go
+++ model/init.go
@@ -1,38 +1,51 @@
 package model
 
 import (
-	// "fmt"
-	// "gopkg.in/mgo.v2"
-	// "gopkg.in/mgo.v2/bson"
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"log"
-	// "os"
+	"os"
+	"strings"
 )
 
 func init() {
 
 	log.Println("Initializing Model Layer")
+	var mongoURL string
 
-	// session, err := mgo.Dial("meetgrinder-db")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer session.Close()
+	// Use a docker-style environment variable to locate the mongo instance
+	// otherwise, fall back and assume localhost
+	tcpURL := os.Getenv("DB_PORT")
+	if tcpURL == "" {
+		mongoURL = "mongodb://localhost"
+	} else {
+		mongoURL = strings.Replace(tcpURL, "tcp://", "mongodb://", -1)
+	}
 
-	// // Optional. Switch the session to a monotonic behavior.
-	// session.SetMode(mgo.Monotonic, true)
+	log.Println("Connecting to mongo at", mongoURL)
 
-	// c := session.DB("test").C("people")
-	// err = c.Insert(&Person{Name: "Ale"},
-	// 	&Person{Name: "Cla"})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	session, err := mgo.Dial(mongoURL)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
 
-	// result := Person{}
-	// err = c.Find(bson.M{"name": "Ale"}).One(&result)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
 
-	// fmt.Println("Name:", result.Name)
+	c := session.DB("test").C("people")
+	err = c.Insert(&Person{Name: "Ale"},
+		&Person{Name: "Cla"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Name:", result.Name)
 }
